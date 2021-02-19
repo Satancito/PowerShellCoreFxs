7e0acc6d3226770fd39e41e260b236e4046db830
Joma <megamanx@outlook.com>
2021-02-19 13:00:57 -0500

    1.0.7.0

--- X-PushMe.ps1
+++ X-PushMe.ps1
@@ -1,11 +1,4 @@
 
-[CmdletBinding()]
-param (
-    [Parameter()]
-    [Switch]
-    $Auto
-)
-
 $ErrorActionPreference = "Stop"
 Import-Module -Name "$(Get-Item "./Z-CoreFxs*.ps1")" -Force -NoClobber
 
@@ -18,19 +11,13 @@ Write-InfoMagenta "███ Commit and push "
 Write-Host
 Write-InfoBlue "█ Commit"
 
-if($Auto.IsPresent)
-{
-    $message = Update-Version -ProjectFileName $project -ValueType $BuildValueType
-}
-else {
-    Write-InfoGreen "Enter a commit message: " -NoNewLine
-    $message = [System.Console]::ReadLine();
-}
-
+Write-InfoGreen "Enter a commit message: " -NoNewLine
+$message = [System.Console]::ReadLine();
 git add -A
 Test-LastExitCode
 git status 
 git commit -m "$message"
+
 Write-Host
 Write-InfoBlue "█ Push to remote"
 git push
