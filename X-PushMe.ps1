42a373c491aa07ce88855baaf13f3c6dab539725
Joma <megamanx@outlook.com>
2021-02-19 12:33:08 -0500

    1.0.2.0

---
+++ X-PushMe.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = "Stop"
+Import-Module -Name "$(Get-Item "./Z-CoreFxs*.ps1")" -Force -NoClobber
+
+Clear-Host
+    
+Write-Host
+Write-InfoMagenta "███ Commit and push "
+Write-InfoBlue "█ Staging files"
+git add -A
+Test-LastExitCode
+git status 
+
+Write-Host
+Write-InfoBlue "█ Commit"
+Write-InfoGreen "Enter a commit message: " -NoNewLine
+$message = [System.Console]::ReadLine();
+git commit -m "$message"
+
+Write-Host
+Write-InfoBlue "█ Push to remote"
+git push
+
+Write-Host
+Write-Host
+Write-InfoMagenta "███ Finished all actions"
