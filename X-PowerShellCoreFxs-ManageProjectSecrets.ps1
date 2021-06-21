251f947921fcb7009f1b5cc886a6cf208e811415
Joma <megamanx@outlook.com>
2021-06-21 05:40:59 -0500

    1.0.123

--- X-PowerShellCoreFxs-ManageProjectSecrets.ps1
+++ X-PowerShellCoreFxs-ManageProjectSecrets.ps1
@@ -1,10 +1,15 @@
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = "seta")]
 param (
-    [string]
-    [ValidateSet("Edit", "Set", "List")]
-    $Action = "List",
+    [Parameter(ParameterSetName = "seta")]
+    [switch]
+    $Edit,
+
+    [Parameter(ParameterSetName = "setb")]
+    [switch]
+    $Set,
     
     [string]
+    [Parameter(ParameterSetName = "seta")]
     $Editor = "code"
 )
 
@@ -13,41 +18,39 @@ Import-Module -Name "$(Get-Item "./Z-CoreFxs*.ps1")" -Force -NoClobber
 $ProjectName = "$(Get-Item -Path "./*.csproj" | Split-Path -Leaf)"
 $SecretsFileName = "$(Get-UserHome)/$ProjectName.Secrets.json"
 
-switch ($Action) {
-    ("Edit") { 
-        Write-PrettyKeyValue "███ Opening secrets for project" "`"$($ProjectName)`""
-        Write-PrettyKeyValue "SecretFilename" "$SecretsFileName"
+if ($Edit.IsPresent) { 
+    Write-PrettyKeyValue "███ Opening secrets for project" "`"$($ProjectName)`""
+    Write-PrettyKeyValue "SecretFilename" "$SecretsFileName"
 
-        if (!(Test-Path -Path $secretsFileName -PathType Leaf)) {
-            New-Item -Path $secretsFileName -Value "{}" | Out-Null
-        }
-        & $editor $SecretsFileName 
+    if (!(Test-Path -Path $secretsFileName -PathType Leaf)) {
+        New-Item -Path $secretsFileName -Value "{}" | Out-Null
     }
-    ("Set") {  
-
-        Write-PrettyKeyValue "███ Setting up secrets for project" "`"$($ProjectName)`""
-        Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
-
-        if (!(Test-Path -Path $SecretsFileName -PathType Leaf)) {
-            Write-Host
-            Write-InfoBlue "█ Creating secrets file"
-            Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
-            New-Item -Path $SecretsFileName -Value "{}"
-        }
-
-        Write-Host
-        Write-InfoBlue "█ Adding secrets" 
-        Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
-        dotnet add "$ProjectName" package "Microsoft.Extensions.Configuration.UserSecrets"
-        dotnet user-secrets init
-        dotnet user-secrets clear
-        Get-Content "$SecretsFileName" | dotnet user-secrets set
-    
-        
-    }
-    Default {
-        Write-InfoBlue "█ Listing secrets"
-        dotnet user-secrets list
-    }
+    & $editor $SecretsFileName 
+    return
 }
+
+if ($Set.IsPresent) {  
+
+    Write-PrettyKeyValue "███ Setting up secrets for project" "`"$($ProjectName)`""
+    Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
+
+    if (!(Test-Path -Path $SecretsFileName -PathType Leaf)) {
+        Write-Host
+        Write-InfoBlue "█ Creating secrets file"
+        Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
+        New-Item -Path $SecretsFileName -Value "{}"
+    }
+
+    Write-Host
+    Write-InfoBlue "█ Adding secrets" 
+    Write-PrettyKeyValue "SecretsFilename" "$SecretsFileName"
+    dotnet add "$ProjectName" package "Microsoft.Extensions.Configuration.UserSecrets"
+    dotnet user-secrets init
+    dotnet user-secrets clear
+    Get-Content "$SecretsFileName" | dotnet user-secrets set
+    return   
+}
+    
+Write-InfoBlue "█ Listing secrets"
+dotnet user-secrets list
 
